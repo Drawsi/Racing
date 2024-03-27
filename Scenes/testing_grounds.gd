e170cb585502d0a4c282867625769675abd6b33c
Emanuel Udrea <drawsiyt@gmail.com>
2024-03-27 14:03:49 +0200

    1.4.1

---
+++ Scenes/testing_grounds.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var track = get_node("StaticBody2D3/Line2D3")
+@onready var out_border = get_node("StaticBody2D3/CollisionPolygon2D")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
