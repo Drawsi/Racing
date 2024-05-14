c6d67c21212d6a7310361ddda3eeb787b13963b8
Emanuel Udrea <drawsiyt@gmail.com>
2024-05-14 14:27:07 +0300

    1.4.3
    Added zoom in and out functions, steering angle changes depending on velocity.length()

--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var wheel_base = 70
 var steering_angle = 8
+var max_steering_angle = 10
 var engine_power = 1800
 var friction = -30
 var drag = -0.06
@@ -14,7 +15,9 @@ var traction_slow = 18
 var acceleration = Vector2.ZERO
 var steer_direction
 
-
+var zoom_speed = 0.05
+var min_zoom = 0.5
+var max_zoom = 1.0
 
 func _physics_process(delta):
 	acceleration = Vector2.ZERO
@@ -24,14 +27,15 @@ func _physics_process(delta):
 	particles()
 	velocity += acceleration * delta
 	move_and_slide()
-	
+	adjust_camera_zoom(delta)
+
 func apply_friction(delta):
 	if acceleration == Vector2.ZERO and velocity.length() < 50:
 		velocity = Vector2.ZERO
 	var friction_force = velocity * friction * delta
 	var drag_force = velocity * velocity.length() * drag * delta
 	acceleration += drag_force + friction_force
-	
+
 func get_input():
 	var turn = Input.get_axis("a", "d")
 	steer_direction = turn * deg_to_rad(steering_angle)
@@ -40,24 +44,40 @@ func get_input():
 	elif Input.is_action_pressed("w"):
 		acceleration = transform.x * engine_power
 		$AudioStreamPlayer2D.play()
-	
+
+func adjust_camera_zoom(delta):
+	var zoom_factor = $Camera2D.zoom.x
+	if velocity.length() < 200 and acceleration == Vector2.ZERO:  # Car is moving slowly and no acceleration
+		zoom_factor = min(max_zoom, zoom_factor + zoom_speed * delta)
+	else:
+		zoom_factor = max(min_zoom, zoom_factor - zoom_speed * delta)
+	$Camera2D.zoom = Vector2(zoom_factor, zoom_factor)
+
 func calculate_steering(delta):
+	var target_steering_angle = steering_angle
+	
+	if velocity.length() > 500:
+		target_steering_angle = max_steering_angle
+	else:
+		target_steering_angle = steering_angle
+		
+	steering_angle = lerp(float(steering_angle), float(target_steering_angle), 0.2)
+	
 	var rear_wheel = position - transform.x * wheel_base / 2.0
 	var front_wheel = position + transform.x * wheel_base / 2.0
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(steer_direction) * delta
 	var new_heading = rear_wheel.direction_to(front_wheel)
 	var traction = traction_slow
-	
+
 	if velocity.length() > slip_speed:
 		traction = traction_fast
-	
+
 	var d = new_heading.dot(velocity.normalized())
 	if d > 0:
 		velocity = lerp(velocity, new_heading * velocity.length(), traction * delta)
 	if d < 0:
 		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
-#	velocity = new_heading * velocity.length()
 	rotation = new_heading.angle()
 
 func particles():
